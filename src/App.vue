c6b628c0c998ac30753844bb76396265dcf561c2
Igor Souza <igormcsouza@gmail.com>
2020-06-15 11:15:17 -0300

    starting to build main page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id=app>
     <stake-menu-bar />
+    <stake-home />
   </div>
 </template>
   
 <script>
 import StakeMenuBar from './components/MenuBar.vue'
+import StakeHome from './components/home/main.vue'
 
 export default {
   name: 'App',
   components: {
-    StakeMenuBar
+    StakeMenuBar, 
+    StakeHome
   }
 }
 </script>
