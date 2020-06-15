57e2d13cb7bb0d906ae68847ee892a158de149d2
Igor Souza <igormcsouza@gmail.com>
2020-06-15 00:38:02 -0300

    added menubar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id=app>
+    <stake-menu-bar />
   </div>
 </template>
-
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StakeMenuBar from './components/MenuBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StakeMenuBar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
