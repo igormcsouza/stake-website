5ac48e9362f172f3819f8163c67ebd774180f148
Igor <igormcsouza@gmail.com>
2020-06-18 20:11:56 -0300

    changed message location

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 
 import StakeHome from './../views/home/Home'
 import StakeAgenda from './../views/agenda/Agenda'
-import StakeMessages from './../views/messages/Messages.vue'
+import StakeMessages from './../views/messages/Messages'
 
 Vue.use(VueRouter)
 
