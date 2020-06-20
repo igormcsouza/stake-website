38ea4fb47b95068e947528731ff2efbe6a366bb7
Igor <igormcsouza@gmail.com>
2020-06-19 23:24:41 -0300

    putting a header on HOME

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<stake-header />
 		<stake-news />
 		<stake-agenda />
 	</div>
@@ -8,12 +9,14 @@
 <script>
 import StakeNews from "./news/News.vue"
 import StakeAgenda from "./agenda/Agenda.vue"
+import StakeHeader from "./Header.vue"
 
 export default {
   name: "StakeHome",
   components: {
 		StakeNews,
-		StakeAgenda
+		StakeAgenda,
+		StakeHeader
   }
 };
 </script>
