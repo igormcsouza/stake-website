724756d8bf94436583e456ef5602988fb696e550
Igor <igormcsouza@gmail.com>
2020-06-21 12:07:03 -0300

    hidding news for this production set

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -25,9 +25,9 @@
         <li class="nav-item">
           <router-link class="nav-link" to="/messages">Mensagens</router-link>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <router-link class="nav-link" to="/newsletter">News</router-link>
-        </li>
+        </li> -->
       </ul>
     </div>
   </nav>
